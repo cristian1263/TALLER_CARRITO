dfc0c4319800cb920d1f429684cc70fbfa65d9f0
Cristian Hernández <cristian.camilo1560@gmail.com>
2022-11-19 03:17:27 -0500

    Cuarto Commit

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
+
 document.addEventListener('DOMContentLoaded', () => {
   fetchData()
 })
@@ -14,4 +19,16 @@ const fetchData = async () => {
   } catch (error) {
     console.log(error)
   }
+}
+
+const pintarCards = data => {
+  data.forEach(producto => {
+    templateCard.querySelector('h5').textContent = producto.name
+    templateCard.querySelector('p').textContent = producto.id * '200'
+    templateCard.querySelector('img').setAttribute('src', producto.img_url)
+    templateCard.querySelector('.btn-dark').dataset.id = producto.id
+    const clone = templateCard.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  items.appendChild(fragment)
 }
